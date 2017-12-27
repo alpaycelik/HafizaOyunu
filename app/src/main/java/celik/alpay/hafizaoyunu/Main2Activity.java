f92028b9fb550749ab48a8696da594a7f624fa30
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-12-28 00:37:44 +0200

    Activity, Intent, Mesaj geçirme.

---
+++ app/src/main/java/celik/alpay/hafizaoyunu/Main2Activity.java
@@ -0,0 +1,19 @@
+package celik.alpay.hafizaoyunu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Intent i = getIntent();
+        String s = i.getStringExtra("mesaj");
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(s);
+    }
+}
