405adda820ef272f97e98a93bace62c2496b7478
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-12-28 12:50:42 +0200

    buton dizisi oluşturuldu, kartlar karıştırıldı.

--- app/src/main/java/celik/alpay/hafizaoyunu/Kart.java
+++ app/src/main/java/celik/alpay/hafizaoyunu/Kart.java
@@ -9,10 +9,42 @@ public class Kart extends Button{
     boolean acikMi = false;
     int resimID;
     int arkaPlanID;
-    public Kart(Context context) {
+    int onPlanID;
+    Drawable arka;
+    Drawable on;
+    public Kart(Context context, int id) {
         super(context);
         arkaPlanID = R.drawable.arkaplan;
-        Drawable d = AppCompatDrawableManager.get().getDrawable(context,arkaPlanID);
-        setBackground(d);
+
+        if(id%8 == 1)
+            onPlanID = R.drawable.c1;
+        if(id%8 == 2)
+            onPlanID = R.drawable.c2;
+        if(id%8 == 3)
+            onPlanID = R.drawable.c3;
+        if(id%8 == 4)
+            onPlanID = R.drawable.c4;
+        if(id%8 == 5)
+            onPlanID = R.drawable.c5;
+        if(id%8 == 6)
+            onPlanID = R.drawable.c6;
+        if(id%8 == 7)
+            onPlanID = R.drawable.c7;
+        if(id%8 == 0)
+            onPlanID = R.drawable.c8;
+
+        arka = AppCompatDrawableManager.get().getDrawable(context,arkaPlanID);
+        on = AppCompatDrawableManager.get().getDrawable(context, onPlanID);
+        setBackground(arka);
+    }
+    public void cevir(){
+        if(!acikMi){ // arkası cevriliyse
+            setBackground(on);
+            acikMi = true;
+        }
+        else{
+            setBackground(arka);
+            acikMi = false;
+        }
     }
 }
--- app/src/main/java/celik/alpay/hafizaoyunu/Main2Activity.java
+++ app/src/main/java/celik/alpay/hafizaoyunu/Main2Activity.java
@@ -3,6 +3,7 @@ package celik.alpay.hafizaoyunu;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridLayout;
 import android.widget.TextView;
 
@@ -17,7 +18,26 @@ public class Main2Activity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.textView);
         tv.setText(s);
         GridLayout gl = (GridLayout) findViewById(R.id.kartlar);
-        for (int j = 0; j<10; j++)
-            gl.addView(new Kart(this));
+        Kart kartlar[] = new Kart[16];
+        for (int j = 1; j<=16; j++) {
+            kartlar[j-1] = new Kart(this, j);
+            kartlar[j-1].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   Kart k = (Kart) v;
+                   k.cevir();
+                }
+            });
+        }
+        // Karıştır
+        for(int j=0; j<16; j++){
+            int rg = (int)(Math.random()*16);
+            Kart k = kartlar[rg];
+            kartlar[rg] = kartlar[j];
+            kartlar[j] = k;
+        }
+        for (int j=0; j<16; j++) {
+            gl.addView(kartlar[j]);
+        }
     }
 }
